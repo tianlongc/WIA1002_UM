28b3eafbd4820f3a0d0b1254b3f871aa5c944a8c
tianlongc <tianlongchoo@gmail.com>
2026-02-10 16:57:50 +0800

    Add custom binary heap priority queue, refactored solution for Q2abc

---
+++ PYQ/WIA1002_2_2025/Q2ex/GenericCircularQueue.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Q2ex;
+
+/**
+ *
+ * @author tianlongc
+ */
+
+/*
+    Question (a): Implement Circular Generic Queue using Array
+                - Methods: constructor, enqueue, dequeue, toString
+                - Maximum capacity is 8 and no resize
+                - Oldest elements will be replaced when the queue is full
+*/
+
+/**
+ * @param <E> the generic type parameter named element
+ */
+public class GenericCircularQueue<E>{
+    private E[] queue;
+    private int front, rear, size, capacity;
+    private final static int MAX_CAPACITY = 8;
+    
+    public GenericCircularQueue(){
+        this(MAX_CAPACITY);
+    }
+    
+    public GenericCircularQueue(int capacity){
+        // generic array creation with safe type cast using Bag concept
+        @SuppressWarnings("unchecked")
+        E[] temp = (E[]) new Object[capacity];
+        this.queue = temp;
+        this.capacity = capacity;
+        this.front = 0;
+        this.rear = 0;
+        this.size = 0;
+    }
+    
+    public boolean isFull(){
+        return size >= capacity;
+    }
+    
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    
+    public void enqueue(E e){
+        if (isFull()) {
+            System.out.println("Queue is full! Replaced " + dequeue());
+        }
+        queue[rear] = e;
+        rear = (rear + 1) % capacity;
+        size++;
+    }
+
+    public E peek(){
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue underflows");
+        }
+        return queue[front];
+    }
+    
+    public E dequeue(){
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue underflows");
+        }
+        E temp = queue[front];
+        queue[front] = null; // dereference
+        front = (front + 1) % capacity;
+        size--;
+        return temp;
+    }
+    
+    @Override
+    public String toString(){
+        if (isEmpty()) {
+            return ""; // early exit
+        }
+        StringBuilder sb = new StringBuilder();
+        // Method 1: Dequeue all elements
+        // while(!isEmpty()){
+        //     sb.append(dequeue()).append(' ');
+        // }
+        
+        // Method 2: Traverse without dequeue
+        for (int i = front, count = 0; count < size; i = (i + 1) % capacity, count++) {
+            sb.append(queue[i]).append(' ');
+        }
+        return sb.toString();
+    }
+}
